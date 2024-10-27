b8c302ef8eef3596393bad427b08c72763acae57
Nikita Fedorin <xfedorin@stuba.sk>
2024-10-27 10:32:02 +0100

    new goal form input handlers

--- frontend/src/GoalForm.js
+++ frontend/src/GoalForm.js
@@ -1,10 +1,32 @@
+import { useState } from 'react';
+
 function GoalForm(){
+    const [currentDate, setCurrentDate] = useState(() => {
+        const today = new Date();
+        return today.toISOString().split('T')[0] || ''; 
+      });
+    const [currentDescription, setCurrentDescription] = useState("");
+
+    function changeDate(event){
+        setCurrentDate(event.target.value);        
+        console.log(event.target.value);
+    }
+
+    function changeDescription(event){
+        setCurrentDescription(event.target.value);
+    }
     return(
         <form id="goalForm">
             <input id="deadlineInput"
-                   type="date"></input>
+                   onChange={changeDate}
+                   type="date"
+                   value={currentDate}>
+            </input>
             <textarea id="descriptionInput"
-                   placeholder="Goal description"></textarea>
+                      onChange={changeDescription}
+                      placeholder="Goal description"
+                      value={currentDescription}>
+            </textarea>
         </form>
     );
 }
