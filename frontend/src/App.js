05ef70f744740dc0243113e6dace2dd419e1ab31
Nikita Fedorin <xfedorin@stuba.sk>
2024-10-30 13:27:49 +0100

    google auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,80 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Menu from './Menu.js';
 import MainArea from './MainArea.js';
 
 function App() {
+    const [user, setUser] = useState("guest");
+    const [token, setToken] = useState("");
     const [goals, setGoals] = useState([]);
     const url_ = 'http://localhost:8000/user/xfedorin/goals';
-    
+
     useEffect(() => {
+        identifyUser(); // Вызываем только один раз при монтировании компонента
         getAllGoals();
     }, []); 
 
-    function getAllGoals() {
-        fetch(url_, {
-            method: 'GET',
-            headers: { 'Content-Type': 'application/json' },
-        })
-        .then(response => response.json())
-        .then(data => setGoals(data))
-        .catch(error => console.error('Error fetching goals:', error));
+    async function identifyUser() {
+        const hash = window.location.hash;
+        const params = new URLSearchParams(hash.substring(1));
+        const accessToken = params.get('access_token');
+        
+        if (accessToken) {
+            console.log("Access Token:", accessToken);
+            setToken(accessToken);
+            setUser("loggedin");
+            window.location.hash = '';
+
+            // Получите информацию о пользователе сразу после получения токена
+            await fetchUserInfo(accessToken);
+        } else {
+            console.log("Токен не найден в URL.");
+        }
     }
+
+    async function fetchUserInfo(accessToken) {
+        try {
+            const response = await fetch('https://www.googleapis.com/oauth2/v3/userinfo', {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`, // Используем переданный токен
+                    'Content-Type': 'application/json'
+                }
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const userInfo = await response.json();
+            const userName = userInfo.name; 
+            setUser(userName); // Установите имя пользователя
+            console.log('User Name:', userName); // Выводим имя пользователя для отладки
+        } catch (error) {
+            console.error('Error fetching user info:', error);
+        }
+    }
+
+    async function getAllGoals() {
+        try {
+            const response = await fetch(url_, {
+                method: 'GET',
+                headers: { 'Content-Type': 'application/json' },
+            });
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            setGoals(data);
+        } catch (error) {
+            console.error('Error fetching goals:', error);
+        }
+    }
+
     return (
         <div id="App-body">
             <header id="App-header">
-                <Menu />
+                <Menu user={user}/>
             </header>
             <MainArea goals={goals} getAllGoals={getAllGoals}/>
         </div>
--- frontend/src/Menu.js
+++ frontend/src/Menu.js
@@ -1,12 +1,22 @@
 import './Menu.css';
 import avatar from "./avatar.png";
 
-function Menu(){
+function Menu({user}){
+    let gAuth = process.env.REACT_APP_GOOGLE_AUTH_URL; 
+    let clientId = process.env.REACT_APP_CLIENT_ID;
+    let redirectUri = process.env.REACT_APP_REDIRECT_URI;
+
+    let fullQuery = gAuth + "?client_id=" + clientId +
+                    "&redirect_uri=" + redirectUri+
+                    "&response_type=token"+
+                    "&scope=openid%20email%20profile";
 return (<nav>
       <ul id="menu">
         <li><a href="#philosofy">Philosofy</a></li>
         <li><a href="#goals">Goals</a></li>
       </ul>
+        <a href={fullQuery}>Google</a>
+        <span>{user}</span>
        <div id="profile">
             <img id="avatarImg" src={avatar} alt="avatar"/>
             <ul id="profileMenu">
