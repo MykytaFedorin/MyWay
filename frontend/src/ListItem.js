c21f0c8e546a9207001da57140c55e6186d04753
Nikita Fedorin <xfedorin@stuba.sk>
2024-11-02 14:25:35 +0100

    list item appearing

--- frontend/src/ListItem.js
+++ frontend/src/ListItem.js
@@ -1,10 +1,14 @@
 import './ListItem.css';
 import bucket from './bucket.png';
+
 function ListItem({ onClick, deleteGoal, goalDescription }) {
+    // Обрезаем строку до первых двух слов
+    const shortDescription = goalDescription.split(" ").slice(0, 2).join(" ");
+    
     return (
         <div className="listItem" onClick={onClick}>
             <li>
-                {goalDescription}
+                {shortDescription}
             </li>
             <img onClick={deleteGoal}
                  src={bucket} alt="deleteBtn"></img>
